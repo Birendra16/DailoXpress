ecff93f9e285c7d3102bbcf5557f73c3f857476f
Birendra Bohara <bbirendra693@gmail.com>
2026-06-01 22:56:33 +0545

    feat: implement NextAuth authentication system
    - Add NextAuth.js configuration and setup (src/auth.ts)
    - Create authentication route handler (src/app/api/auth/[...nextauth]/route.ts)
    - Add TypeScript type definitions for NextAuth (src/next-auth.d.ts)
    - Update dependencies to support authentication workflows
    - Configure NextAuth with required providers and callbacks

---
+++ src/auth.ts
@@ -0,0 +1,71 @@
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import connectDB from "./lib/db"
+import User from "./models/user.model"
+import bcrypt from "bcryptjs"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+        credentials:{
+            email:{label:"email", type:"email"},
+            password:{label:"password", type:"password"},
+        },
+        async authorize(credentials, request) {
+           
+                await connectDB()
+                const email=credentials.email
+                const password=credentials.password as string
+                const user=await User.findOne({email})
+
+                if(!user){
+                    throw new Error("user does not exist")
+                }
+
+                const isMatch=await bcrypt.compare(password,user.password)
+
+                if(!isMatch){
+                    throw new Error("incorrect password")
+                }
+
+                return {
+                    id:user._id.toString(),
+                    email:user.email,
+                    name:user.name,
+                    role:user.role
+                }
+        }
+    })
+  ],
+  callbacks:{
+    // token ko vitra user ko data rakchha
+    jwt({token,user}){
+        if(user){
+            token.id=user.id,
+            token.name=user.name,
+            token.email=user.email
+        }
+        return token
+    },
+
+    session({session,token}){
+        if(session.user){
+            session.user.id=token.id as string,
+            session.user.name=token.name as string,
+            session.user.email=token.email as string,
+            session.user.role=token.role as string
+        }
+        return session
+
+    }
+  },
+  pages:{
+    signIn:"/login",
+    error:"/login"
+  },
+  session:{
+    strategy:"jwt",
+    maxAge:10*24*60*60*1000
+  },
+  secret:process.env.AUTH_SECRET
+})
